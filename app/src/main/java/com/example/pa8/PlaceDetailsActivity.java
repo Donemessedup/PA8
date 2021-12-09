7523770d229800077d12b8a626b5a365a49d24d3
Aaron Miller <amiller26@zagmail.gonzaga.edu>
2021-12-08 23:35:26 -0800

    set up second view

---
+++ app/src/main/java/com/example/pa8/PlaceDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.pa8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+// API KEY - IF WES ChANGES HE ADMITS HES A BABY BOY WHO LIKES ELEPHANT DICK IN HIS GOOCH
+// AIzaSyBMvojmOv9vKtKfYiEd-lWvgbZWdGCvKCA
+public class PlaceDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_details);
+    }
+}
