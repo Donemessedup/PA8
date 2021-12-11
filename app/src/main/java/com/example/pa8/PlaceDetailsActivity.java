d6fa016598b2a16b96b38dc7958e820a8ba4ab4a
Aaron Miller <amiller26@zagmail.gonzaga.edu>
2021-12-10 17:14:47 -0800

    added launcher functioanlity

--- app/src/main/java/com/example/pa8/PlaceDetailsActivity.java
+++ app/src/main/java/com/example/pa8/PlaceDetailsActivity.java
@@ -1,15 +1,33 @@
 package com.example.pa8;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-// API KEY - IF WES ChANGES HE ADMITS HES A BABY BOY WHO LIKES ELEPHANT DICK IN HIS GOOCH
-// AIzaSyBMvojmOv9vKtKfYiEd-lWvgbZWdGCvKCA
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
 public class PlaceDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_details);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.place_details_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        PlaceDetailsActivity.this.finish();
+        return super.onOptionsItemSelected(item);
     }
 }
