577ea8084f3a240d4bb46261d722a606fda74468
Big Wes <wesmuehlhausen@gmail.com>
2021-12-10 18:23:45 -0800

    test2

---
+++ app/src/main/java/com/example/pa8/ConstraintLayoutPlaceDetails.java
@@ -0,0 +1,16 @@
+package com.example.pa8;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class ContraintLayoutPlaceDetails extends ConstraintLayout {
+
+    public ContraintLayoutPlaceDetails(@NonNull Context context) {
+        super(context);
+        
+
+    }
+
+}
